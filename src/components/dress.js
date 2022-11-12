7e5829da5a7856617e8c178e504022da22fd332f
Shawaal-Nadeem <hafizshawalnadeem@gmail.com>
2022-11-12 11:37:19 -0800

    Completed

--- src/components/dress.js
+++ src/components/dress.js
@@ -4,7 +4,152 @@ import React from 'react';
 function Dress() {
   return (
     <div>
-       <p>Dress page</p>
+    <div className='pics'>
+       <img className='pic2-sets' src='/images/2.jpg'/>
+       <img className='pic2-sets' src='/images/3.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/4.jpg'/>  
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/10.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/11.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/12.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/13.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/14.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/15.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic2-sets' src='/images/16.jpg'/>
+       <img className='pic2-sets' src='/images/17.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/18.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/19.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/20.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/21.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic3-sets' src='/images/22.jpg'/>
+       <img className='pic3-sets' src='/images/23.jpg'/>
+       <img className='pic3-sets' src='/images/24.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/25.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic3-sets' src='/images/27.jpg'/>
+       <img className='pic3-sets' src='/images/26.jpg'/>
+       <img className='pic3-sets' src='/images/28.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/29.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic2-sets' src='/images/31.jpg'/>   
+       <img className='pic2-sets' src='/images/32.jpg'/>   
+    </div>
+   
+    <div className='pics'>
+       <img className='pic2-sets' src='/images/33.jpg'/>   
+       <img className='pic2-sets' src='/images/34.jpg'/>
+    </div>
+    <div className='pics'>
+       <img className='pic2-sets' src='/images/35.jpg'/>   
+       <img className='pic2-sets' src='/images/36.jpg'/>  
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/37.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/38.jpg'/> 
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/39.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/40.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/41.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/42.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/43.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/44.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic2-sets' src='/images/45.jpg'/>
+       <img className='pic2-sets' src='/images/46.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic2-sets' src='/images/47.jpg'/>
+       <img className='pic2-sets' src='/images/48.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic2-sets' src='/images/50.jpg'/>
+       <img className='pic2-sets' src='/images/49.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/51.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic2-sets' src='/images/53.jpg'/>
+       <img className='pic2-sets' src='/images/52.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/55.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic3-sets' src='/images/54.jpg'/>
+       <img className='pic3-sets' src='/images/56.jpg'/>
+       <img className='pic3-sets' src='/images/57.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic3-sets' src='/images/58.jpg'/>
+       <img className='pic3-sets' src='/images/59.jpg'/>
+       <img className='pic3-sets' src='/images/60.jpg'/>   
+    </div>
+    <div className='pics'>
+       <img className='pic2-sets' src='/images/61.jpg'/>
+       <img className='pic2-sets' src='/images/62.jpg'/>
+    </div>
+    <div className='pics'>
+       <img className='pic2-sets' src='/images/63.jpg'/>
+       <img className='pic2-sets' src='/images/64.jpg'/>
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/66.jpg'/>
+    </div>
+    <div className='pics'>
+       <img className='pic2-sets' src='/images/65.jpg'/>
+       <img className='pic2-sets' src='/images/67.jpg'/>
+
+    </div>
+    <div className='pics'>
+       <img className='pic1-sets' src='/images/footpic.jpg'/>
+    </div>
+
     </div>
   );
 }
