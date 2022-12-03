3b0100ce7fc38ece312cc46eb13506de632d753d
Shawaal-Nadeem <hafizshawalnadeem@gmail.com>
2022-12-03 13:26:02 +0500

    Update Slideshow

--- src/components/home.js
+++ src/components/home.js
@@ -1,58 +1,24 @@
 import React from 'react';
-import { useEffect } from 'react';
-
+import { Fade } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 function Home() {
-   useEffect(() => {
-  window.addEventListener("load", () => {
-    autoSlide();
- })
- 
- function autoSlide() {
-    setInterval(() => {
-       slide(getItemActiveIndex() + 1);
-    }, 3000); // slide speed = 3s
- }
- 
- function slide(toIndex) {
-    const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
-    const itemActive = document.querySelector(".carousel_item__active");
- 
-    // check if toIndex exceeds the number of carousel items
-    if (toIndex >= itemsArray.length) {
-       toIndex = 0;
-    }
- 
-    const newItemActive = itemsArray[toIndex];
- 
-    // start transition
-    newItemActive.classList.add("carousel_item__pos_next");
-    setTimeout(() => {
-       newItemActive.classList.add("carousel_item__next");
-       itemActive.classList.add("carousel_item__next");
-    }, 20);
- 
-    // remove all transition class and switch active class
-    newItemActive.addEventListener("transitionend", () => {
-       itemActive.className = "carousel_item";
-       newItemActive.className = "carousel_item carousel_item__active";
-    }, {
-       once: true
-    });
- }
- 
- function getItemActiveIndex() {
-    const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
-    const itemActive = document.querySelector(".carousel_item__active");
-    const itemActiveIndex = itemsArray.indexOf(itemActive);
-    return itemActiveIndex;
- }
-});
+      const fadeImages = [
+         "/images/21.jpg",
+         "/images/58.jpg",         
+         "/images/2.jpg",
+         "/images/66.jpg",
+         "/images/57.jpg",
+         "/images/56.jpg",
+         "/images/54.jpg",
+         "/images/20.jpg",
+         "/images/33.jpg",
+             ];
   return (
     <div>
         <div className='logo-image'>
      <img src='/images/cout.png'/>
      </div>
-     <div class="carousel">
+     {/* <div class="carousel">
       <div class="carousel_inner">
          <div class="carousel_item carousel_item__active">
             <img src="./images/21.jpg" alt="" class="carousel_img"/>
@@ -82,7 +48,38 @@ function Home() {
             <img src="/images/33.jpg" alt="" class="carousel_img"/>
          </div>
       </div>
-   </div>
+   </div> */}
+    <div className="slide-container">
+      <Fade>
+        <div className="each-fade">
+          <img src={fadeImages[0]} />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[1]} />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[2]} />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[3]} />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[4]} />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[5]} />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[6]} />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[7]} />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[8]} />
+        </div>
+      </Fade>
+    </div>
     </div>
   );
 }
